2fa7037a9b67311598aa91e7abc00b0a22e7dbfb
NameIsFill <filpopowski@gmail.com>
2023-05-18 23:19:06 +0200

    added photo gallery

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,57 @@
 import { useState, useEffect } from 'react';
+import { ToggleButton, ToggleButtonGroup} from "@mui/material";
+
 
 export function Dashboard() {
+    const [formats, setFormats] = useState();
+    const [dashboardView, setDashboardView] = useState();
+
+    useEffect((dashboardView) => {
+        if(dashboardView === 1) {
+            return (
+                <div>
+                    <PostsDashboard />
+                </div>
+            )
+        }
+        if(dashboardView === 2) {
+            return (
+                <div>
+                    <PhotoGallery />
+                </div>
+            )
+        }
+    }, [dashboardView]);
+
+    function handleFormatChange(event, newFormat) {
+        setFormats(newFormat);
+        console.log(newFormat);
+    }
+
+    function handleOnClick(number) {
+        setDashboardView(number)
+    }
+
+    return (
+        <div>
+            <ToggleButtonGroup value={formats} exclusive onChange={handleFormatChange}>
+                <ToggleButton value="1" variant="contained" onClick={ () => handleOnClick(1) }>
+                    Posts
+                </ToggleButton>
+                <ToggleButton value="2" variant="contained" onClick={ () => handleOnClick(2) }>
+                    Photos
+                </ToggleButton>
+            </ToggleButtonGroup>
+        </div>
+
+    )
+}
+
+function PhotoGallery() {
+
+}
+
+function PostsDashboard() {
     const [userId, setUserId] = useState(null);
 
     function handleOnClick(i) {
@@ -17,6 +68,7 @@ export function Dashboard() {
 
 function UserButtons(props) {
     const [usersData, setUsersData] = useState();
+    const [formats, setFormats] = useState();
 
     useEffect(() => {
         async function fetchData() {
@@ -35,15 +87,22 @@ function UserButtons(props) {
         fetchData();
     }, []);
 
+    function handleFormatChange(event, newFormat) {
+        setFormats(newFormat);
+        console.log(newFormat);
+    }
+
     if (!usersData) {
         return null;
     }
 
     return (
         <div>
-            {usersData.map((user) =>
-                <button onClick={ () => props.handleClick(user.id) } key={ user.id }>{ user.name }</button>
-            )}
+            <ToggleButtonGroup value={formats} exclusive onChange={handleFormatChange}>
+                {usersData.map((user) =>
+                    <ToggleButton value={user.id} variant="contained" onClick={ () => props.handleClick(user.id) } key={ user.id }>{ user.name }</ToggleButton>
+                )}
+            </ToggleButtonGroup>
         </div>
     )
 }
